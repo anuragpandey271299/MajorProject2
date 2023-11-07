ad1903b0f4ab71c91eacedacc62c655745fb7a75
anuragCodes <anurag.info27@gmail.com>
2023-11-08 02:48:41 +0530

    Added few more styling

--- pocket-notes/src/Components/LeftSide.js
+++ pocket-notes/src/Components/LeftSide.js
@@ -2,16 +2,21 @@ import React, { useState, useEffect, useRef } from 'react';
 import CreateNotes from './CreateNotes';
 import './LeftSide.css';
 
-function LeftSide({onProfileClick}) {
-    
-
+function LeftSide({ onProfileClick }) {
   const [showCreateNotes, setShowCreateNotes] = useState(false);
   const containerRef = useRef(null);
 
+  const storedNotes = JSON.parse(localStorage.getItem('notesData'));
+  console.log(storedNotes);
+
+  const [selectedProfile, setSelectedProfile] = useState(null);
+
+  const handleProfileClick = (id) => {
+    setSelectedProfile(id);
+  }
   const handleCreateNotes = () => {
     setShowCreateNotes(true);
   }
-  
 
   useEffect(() => {
     const handleContainerClick = (event) => {
@@ -31,28 +36,31 @@ function LeftSide({onProfileClick}) {
     };
   }, [showCreateNotes]);
 
-  const storedNotes = JSON.parse(localStorage.getItem('notesData'));
-  console.log(storedNotes)
 
   return (
-      <div ref={containerRef} className='wrapperLeftSide' >
-        <h1>Pocket Notes</h1>
+    <div ref={containerRef} className='wrapperLeftSide' >
+      <h1>Pocket Notes</h1>
       <button onClick={handleCreateNotes}>Create Note Group</button>
       {showCreateNotes && <CreateNotes setShowCreateNotes={setShowCreateNotes} />}
       <div className='NoteGroups'>
-          {storedNotes && storedNotes.slice().reverse().map((note) => (
-            <div className='profile' onClick={()=>onProfileClick(note.groupName, note.buttonClicked, note.id)}>
-            <div className='logo' style={{backgroundColor:note.buttonClicked}}>
-            {note.groupName.length >= 2 ? (
+        {storedNotes && storedNotes.slice().reverse().map((note) => (
+          <div
+            className={`profile ${selectedProfile === note.id ? 'selected' : ''}`}
+            onClick={() => {
+              onProfileClick(note.groupName, note.buttonClicked, note.id);
+              handleProfileClick(note.id);
+            }}
+          >
+            <div className='logo' style={{ backgroundColor: note.buttonClicked }}>
+              {note.groupName.length >= 2 ? (
                 <span>{note.groupName[0]}{note.groupName.slice(-1)}</span>
               ) : (
                 <span>{note.groupName}</span>
               )}
             </div>
-            <div className='head'><h1>{note.groupName.slice(0,15)}</h1></div>
-              
-              </div>
-          ))}
+            <div className='head'><h1>{note.groupName.slice(0, 15)}</h1></div>
+          </div>
+        ))}
       </div>
     </div>
   );
